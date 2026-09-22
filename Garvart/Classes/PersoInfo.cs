2b2f56a25bf2a475b99ddf38f753a4c569b29615
irinagarvart1205@gmail.com <irinagarvart1205@gmail.com>
2026-09-22 14:21:02 +0500

    Разработанный класс. Гарварт.

---
+++ Garvart/Classes/PersoInfo.cs
@@ -0,0 +1,24 @@
+﻿namespace Garvart.Classes
+{
+    public class PersoInfo
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Armor { get; set; }
+        public int Level { get; set; }
+        public int Glasses { get; set; }
+        public int Money { get; set; }
+        public float Dmage {  get; set; }
+        public PersoInfo(string Name, int Health, int Armor, int Level, int Glasses, int Money, float Dmage)
+        {
+            this.Name = Name;
+            this.Health = Health;
+            this.Armor = Armor;
+            this.Level = Level;
+            this.Glasses = Glasses;
+            this.Money = Money;
+            this.Dmage = Dmage;
+        }
+    }
+    
+}
--- Garvart/MainWindow.xaml.cs
+++ Garvart/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Garvart.Classes;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace Garvart
 {
@@ -20,6 +9,7 @@ namespace Garvart
     /// </summary>
     public partial class MainWindow : Window
     {
+        public Classes.PersoInfo Player = new Classes.PersoInfo("Student", 100,10, 1, 0, 0, 5 );
         public MainWindow()
         {
             InitializeComponent();
